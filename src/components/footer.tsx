622ccb9e7771eeb9743c74cc86ef77122c8392c3
Kevin Barreto <barretokevin@hotmail.com>
2025-03-11 20:20:27 -0600

    feat: :sparkles: add footer and contact form

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import ContactForm from './contact-form';
 
 function Footer() {
   return (
-    <footer className="bg-white border-t border-gray-200 py-16">
+    <footer id="contact" className="bg-white border-t border-gray-200 py-16">
       <div className="container mx-auto px-6">
         <div className="text-center mb-12">
           <h2 className="text-5xl font-black md:text-7xl mb-4">Get in Touch</h2>
